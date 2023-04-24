91db18db03921e91248061e2b9aadb7d8fd346a8
user <halikovamadina656@gmail.com>
2023-04-24 17:13:10 +0600

    method

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoHibernateImpl.java
@@ -1,62 +1,75 @@
 package peaksoft.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import peaksoft.model.User;
 import peaksoft.util.Util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-
+    EntityManagerFactory entityManagerFactory = Util.getEntityManager();
     public UserDaoHibernateImpl() {
 
     }
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSession().openSession();
-        session.beginTransaction();
-
-
     }
 
     @Override
     public void dropUsersTable() {
-
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.createQuery("delete from users").executeUpdate();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        System.out.println("Table 'users' is dropped");
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        SessionFactory session1 = Util.getSession();
-        Session session = session1.openSession();
-        session.beginTransaction();
-        session.persist(name);
-        session.persist(lastName);
-        session.persist(age);
-        session.getTransaction().commit();
-        session.close();
-        System.out.println(name + "is added to database");
-
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        User user = new User();
+        user.setName(name);
+        user.setLastName(lastName);
+        user.setAge(age);
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public void removeUserById(long id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        User user = entityManager.find(User.class,id);
+        entityManager.remove(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        System.out.println(user.getName() + " is deleted!!!");
 
     }
 
     @Override
     public List<User> getAllUsers() {
-//        Session session = Util.getSession().openSession();
-//        session.beginTransaction();
-//        List<User> students =session.createQuery("select u from User u").getResultList();
-//        session.getTransaction().commit();
-//        session.close();
-//        return students ;
-        return null;
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<User>user = entityManager.createQuery("select u from users u ",User.class).getResultList();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return user;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        Query query = entityManager.createQuery("delete from users ");
+        query.executeUpdate();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -1,15 +1,39 @@
 package peaksoft.util;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import peaksoft.model.User;
 
+import javax.persistence.EntityManagerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
+    public static EntityManagerFactory getEntityManager() {
+        try {
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, "org.postgresql.Driver");
+            properties.put(Environment.URL, "jdbc:postgresql://localhost:5432/postgres");
+            properties.put(Environment.USER, "postgres");
+            properties.put(Environment.PASS, "postgres");
+            properties.put(Environment.HBM2DDL_AUTO, "create");
+            properties.put(Environment.DIALECT,"org.hibernate.dialect.PostgreSQLDialect");
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.FORMAT_SQL, "true");
+
+            Configuration configuration = new Configuration();
+            configuration.addProperties(properties);
+            configuration.addAnnotatedClass(Util.class);
+            System.out.println("Successfully connected!");
+            return configuration.buildSessionFactory().unwrap(EntityManagerFactory.class);
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 
     private static final String url = "jdbc:postgresql://localhost:5432/postgres";
     private static final String userName = "postgres";
